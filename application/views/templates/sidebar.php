2c1783865589246131f98e7c589d46f1dfea704c
anggitadianing <anggitadianing@gmail.com>
2021-12-17 19:26:25 +0700

    pertemuan11

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -1,43 +1,43 @@
 <!-- Sidebar -->
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-<!-- Sidebar - Brand -->
-<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-    <div class="sidebar-brand-icon rotate-n-15">
-        <i class="fas fa-laugh-wink"></i>
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Pustaka Booking </div>
+    </a>
+
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Master Data
     </div>
-    <div class="sidebar-brand-text mx-3">Pustaka Booking </div>
-</a>
-
-
-<!-- Divider -->
-<hr class="sidebar-divider">
-
-<!-- Heading -->
-<div class="sidebar-heading">
-    Master Data
-</div>
 
 
 
-
-<!-- Nav Item - Dashboard -->
-<li class="nav-item">
-  <style media="screen">
-
-  </style>
-    <a class="nav-link pb-0" href="<?= base_url('Buku/kategori'); ?>">
-       <i class="fas fa-book-open"></i>
-        <span>Kategori Buku</span></a>
-</li>
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item">
-      <style media="screen">
+        <style media="screen">
 
-      </style>
-        <a class="nav-link pb-0" href="<?= base_url('buku'); ?>">
-           <i class="fas fa-book"></i>
+        </style>
+        <a class="nav-link pb-0" href="<?= base_url('Buku/kategori'); ?>">
+            <i class="fas fa-book-open"></i>
+            <span>Kategori Buku</span></a>
+    </li>
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item">
+        <style media="screen">
+
+        </style>
+        <a class="nav-link pb-0" href="<?= base_url('buku/kategori'); ?>">
+            <i class="fas fa-book"></i>
             <span>Data Buku</span></a>
     </li>
 
@@ -48,18 +48,18 @@
     </li>
 
 
-</li>
+    </li>
 
 
 
 
-<!-- Divider -->
-<hr class="sidebar-divider mt-3">
+    <!-- Divider -->
+    <hr class="sidebar-divider mt-3">
 
-<!-- Sidebar Toggler (Sidebar) -->
-<div class="text-center d-none d-md-inline">
-     <button class="rounded-circle border-0" id="sidebarToggle"></button>
- </div>
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
 
 
 </ul>
